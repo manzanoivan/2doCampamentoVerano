9837a1accc4271ea2c92f9f54f4b910304b549e0
manzanoivan95 <ivan_ajedrez@hotmail.com>
2017-07-04 11:56:17 -0500

    Matemagias

---
+++ EuclidesExtendido.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+typedef long long Long;
+// Algoritmo de Euclides extendido entre a y b. Ademas de devolver el gcd(a, b), resuelve la identidad de Bezout con el par (x, y).
+//Si el parametro mod no es especificado, se resuelve con aritmetica normal; si mod se especifica, la identidad se resuelve modulo mod.
+Long Euclides(Long a, Long b, Long& x,
+			  Long& y, Long mod = 0) {
+	if (!b) { x = 1, y = 0; return a; }
+	Long gcd = Euclides(b, a % b, x, y, mod);
+	x = !mod? x - y * (a / b): 
+			 (mod + x - (y * (a / b)) % mod) % mod;
+	swap(x, y);
+	return gcd;
+}
+
+int main(){
+
+}
